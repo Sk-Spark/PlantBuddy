4003b98e1d0b24aac9816c155380db3b5faafeb6
Aaliya Islam <aaislam@microsoft.com>
2023-08-04 14:17:59 +0530

    Revert "ui"
    This reverts commit b36191581722fc67dd9af828b110c68e0c5c7a0f.

--- AzureFunction/ServiceBusTopicTrigger1/index.js
+++ AzureFunction/ServiceBusTopicTrigger1/index.js
@@ -6,31 +6,31 @@ module.exports = async function (context, mySbMsg) {
   const { v4: uuidv4 } = require("uuid");
   const { CosmosClient } = require("@azure/cosmos");
   const client = new CosmosClient({ endpoint, key });
-  // const { ApplicationInsights } = require("@microsoft/applicationinsights-web");
-  // const appInsights = new ApplicationInsights({
-  //   config: {
-  //     connectionString: appinsightConnectionString,
-  //     /* ...Other Configuration Options... */
-  //   },
-  // });
+  const { ApplicationInsights } = require("@microsoft/applicationinsights-web");
+  const appInsights = new ApplicationInsights({
+    config: {
+      connectionString: appinsightConnectionString,
+      /* ...Other Configuration Options... */
+    },
+  });
   const { database } = await client.databases.createIfNotExists({
     id: "plants-buddy",
   });
-  // appInsights.loadAppInsights();
-  // appInsights.trackTrace({
-  //   message: "PlantBuddy - Calling SendDataToCosmos Method",
-  // });
+  appInsights.loadAppInsights();
+  appInsights.trackTrace({
+    message: "PlantBuddy - Calling SendDataToCosmos Method",
+  });
 
   // Function call to send data to cosmos
-  sendDataToCosmos(mySbMsg, database, uuidv4(), null);
+  sendDataToCosmos(mySbMsg, database, uuidv4(), appInsights);
 };
 
 async function sendDataToCosmos(data, database, id, appInsights) {
   // function to send data to cosmos db
   try {
-    // appInsights.trackTrace({
-    //   message: "PlantBuddy - In SendDataToCosmos Method",
-    // });
+    appInsights.trackTrace({
+      message: "PlantBuddy - In SendDataToCosmos Method",
+    });
     const dataToCosmos = {
       id,
       deviceId: `${data.deviceId}`,
@@ -49,14 +49,14 @@ async function sendDataToCosmos(data, database, id, appInsights) {
     const { container } = await database.containers.createIfNotExists({
       id: "telemetry_db",
     });
-    // appInsights.trackTrace(
-    //   { message: "PlantsBuddy - Creating Item in Cosmos Container" },
-    //   { data: dataToCosmos }
-    // );
+    appInsights.trackTrace(
+      { message: "PlantsBuddy - Creating Item in Cosmos Container" },
+      { data: dataToCosmos }
+    );
 
     await container.items.create(dataToCosmos);
     console.log("Item created in cosmos");
   } catch (ex) {
-    // appInsights.trackException({ exception: new Error(ex) });
+    appInsights.trackException({ exception: new Error(ex) });
   }
 }
